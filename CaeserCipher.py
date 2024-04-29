070e2283b726f82e5052c86e47cc8e6e7cc56802
BhargaviPydi08 <lakshmibhargavipydi@gmail.com>
2024-04-29 12:28:49 +0530

    Caesar-cipher commit

---
+++ CaeserCipher.py
@@ -0,0 +1,60 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z']
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+#Caesar Function to perfoem encryption and decryption
+def caesar(start_text, shift_no, direction):
+    end_text = ""
+    if direction == "decode":
+            shift_no *= -1
+    for char in start_text:
+        if char in alphabet:
+             position = alphabet.index(char)
+             new_position = position+shift_no
+             end_text += alphabet[new_position]
+        else:
+             end_text += char
+             
+        
+    print(f"The {command}d text is : {end_text}") 
+
+print(logo)
+#Task - 1: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+should_continue = True
+while should_continue:
+
+    command = input("Type 'encode' to encrypt and 'decode' to decrypt")
+    text = input("Enter your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    #Task - 2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+    #Try running the program and entering a shift number of 45.
+    #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+    #Hint: Think about how you can use the modulus (%).
+
+    shift = shift%26
+    caesar(start_text = text, shift_no = shift, direction = command)
+    
+    result = input("Type 'yes' to run again or 'no' to exit")
+    if result == "no":
+         should_continue = False
+         print("GoodBye!")
+
